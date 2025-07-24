1b8e5aac8ea29cb87a8a9b257596a447b6dfc467
LeanDevLima <leanderson.devlima@gmail.com>
2025-07-24 17:40:24 -0300

    Implementação final com provider manual

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,22 @@
-terraform {
-  required_providers {
-    koyeb = {
-      source = "koyeb/koyeb"
-      version = "2.1.0"  # Versão exata que estamos instalando manualmente
-    }
-  }
-}
-
 variable "koyeb_token" {
   type        = string
   sensitive   = true
-  description = "Koyeb API token"
 }
 
 variable "image" {
   type        = string
-  description = "Docker image to deploy (format: repository:tag)"
 }
 
 provider "koyeb" {
   token = var.koyeb_token
 }
 
-resource "koyeb_app" "saudacoes-app" {
+resource "koyeb_app" "app" {
   name = "saudacoes-app-${replace(substr(uuid(), 0, 8), "-", "")}"
 }
 
-resource "koyeb_service" "saudacoes-service" {
-  app_name = koyeb_app.saudacoes-app.name
+resource "koyeb_service" "service" {
+  app_name = koyeb_app.app.name
   name     = "saudacoes-service"
   
   definition {
@@ -45,6 +34,6 @@ resource "koyeb_service" "saudacoes-service" {
   }
 }
 
-output "service_url" {
-  value = "https://${koyeb_app.saudacoes-app.name}.koyeb.app"
+output "url" {
+  value = "https://${koyeb_app.app.name}.koyeb.app"
 }
